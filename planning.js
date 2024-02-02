9b24a5f8d17ff69486264f10494696105c9d345a
ShivamNabara <117414857+ShivamNabara@users.noreply.github.com>
2024-02-02 21:15:02 +0530

    Add files via upload

---
+++ planning.js
@@ -0,0 +1,21 @@
+//Book my show planning
+
+//->We need a resuable carousel
+//->HERO CAROUSEL
+//->COMMON CAROUSEL
+//->CAST CAROUSEL
+
+//Pages
+
+//->Home Page
+//->Movie Page
+//->Categories Page
+//->Auth Page
+//->Razorpay payment gateway
+
+//Plan how we are builiding it ?
+
+//Layouts
+//Routes
+//Pages
+//Components
